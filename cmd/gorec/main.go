a35f95f5fb0ede018211e841cffb73d8a959346f
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-09-05 15:28:45 -0400

    feat: add version command

--- cmd/gorec/main.go
+++ cmd/gorec/main.go
@@ -5,16 +5,26 @@ import (
 	"log"
 	"os"
 
+	"github.com/ajorgensen/gorec/gorec"
 	"github.com/urfave/cli/v2"
 )
+
+func version() *cli.Command {
+	return &cli.Command{
+		Name: "version",
+		Action: func(c *cli.Context) error {
+			fmt.Println(gorec.Version)
+			return nil
+		},
+	}
+}
 
 func main() {
 	app := &cli.App{
 		Name:  "gorec",
 		Usage: "Make http requests with ease",
-		Action: func(*cli.Context) error {
-			fmt.Println("Hello, gorec!")
-			return nil
+		Commands: []*cli.Command{
+			version(),
 		},
 	}
 
